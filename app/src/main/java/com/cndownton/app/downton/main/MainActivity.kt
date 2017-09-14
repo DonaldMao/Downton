b1f2d6d052d76a786bcc17f71c7e553760e4e2ff
donaldmao <donaldmao201@gmail.com>
2017-09-14 14:35:15 +0800

    9.14 白天 权限管理

--- app/src/main/java/com/cndownton/app/downton/main/MainActivity.kt
+++ app/src/main/java/com/cndownton/app/downton/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.cndownton.app.downton.main
 
+import android.Manifest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
+import android.support.v7.app.AlertDialog
 import android.widget.ImageView
+import android.widget.Toast
 import com.cndownton.app.R
 import com.cndownton.app.downton.main.community.CommunityFragment
 import com.cndownton.app.downton.main.home.HomeFragment
@@ -14,7 +17,9 @@ import com.cndownton.app.downton.main.me.MeFragment
 import com.cndownton.app.downton.main.surround.SurroundFragment
 import com.tencent.mm.opensdk.openapi.IWXAPI
 import com.tencent.mm.opensdk.openapi.WXAPIFactory
+import permissions.dispatcher.*
 
+@RuntimePermissions()
 class MainActivity : AppCompatActivity() {
     private lateinit var iv_home_tab: ImageView
     private lateinit var iv_mall_tab: ImageView
@@ -26,9 +31,11 @@ class MainActivity : AppCompatActivity() {
     private val mFragments: ArrayList<Fragment> = ArrayList()
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MainActivityPermissionsDispatcher.getPermissionsWithCheck(this)
 
         iv_home_tab = findViewById(R.id.iv_home_tab)
         vp_main = findViewById(R.id.vp_home)
@@ -150,6 +157,41 @@ class MainActivity : AppCompatActivity() {
             iv_surround_tab -> iv_selected.setImageResource(R.drawable.surround)
             iv_me_tab -> iv_selected.setImageResource(R.drawable.me)
         }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this,requestCode,grantResults)
+    }
+    @NeedsPermission(Manifest.permission.READ_PHONE_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun getPermissions(){
+
+    }
+    @OnShowRationale(Manifest.permission.READ_PHONE_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun showRationaleForContact(request: PermissionRequest) {
+        showRationaleDialog("我们需要权限连接手机中微信，从而能够使用微信登录", request)
+    }
+
+    @OnPermissionDenied(Manifest.permission.READ_PHONE_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    internal fun onDenied() {
+        // NOTE: Deal with a denied permission, e.g. by showing specific UI
+        // or disabling certain functionality
+        Toast.makeText(this, "微信登录功能无法使用", Toast.LENGTH_SHORT).show()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.READ_PHONE_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    internal fun onNeverAskAgain() {
+        Toast.makeText(this,"由于禁止了部分权限，程序功能无法正常使用", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showRationaleDialog(string: String, request: PermissionRequest) {
+        AlertDialog.Builder(this)
+                .setPositiveButton("同意") { _, _ -> request.proceed() }
+                .setNegativeButton("拒绝") { _, _ -> request.cancel() }
+                .setCancelable(false)
+                .setMessage(string)
+                .show()
     }
 }
 
