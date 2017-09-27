f9980c1ce6971ec7e24fd935c683f812d1b0425f
donaldmao <donaldmao201@gmail.com>
2017-09-27 23:15:36 +0800

    9/27

---
+++ app/src/main/java/com/cndownton/app/downton/customer/upgrade/UpgradeActivity.kt
@@ -0,0 +1,12 @@
+package com.cndownton.app.downton.customer.upgrade
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UpgradeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upgrade)
+    }
+}
